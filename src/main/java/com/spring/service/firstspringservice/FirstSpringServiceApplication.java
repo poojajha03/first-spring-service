8678ee48bc78d04e9e14b883cb031bcf0ecebffd
vpoojabhosale <v.pooja.bhosale@red.virgin.com>
2023-10-11 15:45:24 +0100

    Spring Cloud Gateway First Applicaiton

---
+++ src/main/java/com/spring/service/firstspringservice/FirstSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.spring.service.firstspringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FirstSpringServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/spring/service/firstspringservice/FirstSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.service.firstspringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
